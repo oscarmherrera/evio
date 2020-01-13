4ea47fa1bfda77fb2620a3cd8c8e4178177289e9
Oscar M Herrera <oscar@kss-inc.com>
2020-01-12 22:05:04 -0500

    Added GetConn to the all interface

--- eviokjarni.go
+++ eviokjarni.go
@@ -2,4 +2,6 @@ package evio
 
 import "net"
 
-func (c *stdconn) GetConn() net.Conn { return c.conn }
+func (c *stdconn) GetConn() net.Conn    { return c.conn }
+func (c *stdudpconn) GetConn() net.Conn { return nil }
+func (c *conn) GetConn() net.Conn       { return nil }
